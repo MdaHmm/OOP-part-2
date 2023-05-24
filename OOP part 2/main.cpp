c9f1f6e79275ea1a4734a1033559f0f82d1f0945
DevC <dimabmwmw@mail.ru>
2023-05-24 21:22:32 +0300

    Практика по ООП

--- OOP part 2/main.cpp
+++ OOP part 2/main.cpp
@@ -92,11 +92,57 @@ public:
 	}
 };
 
+class Client {
+private:
+	std::string _name;
+	int _age;
+	double _bill;
+public:
+	Client(std::string name, int age, double bill){
+		_name = name;
+		_age = age;
+		_bill = bill;
+	}
+
+	void ShowData() {
+		std::cout << "Имя клиента: " << _name << std::endl;
+		std::cout << "Возраст клиента: " << _age << std::endl;
+		std::cout << "Состояние счета клиента: " << _bill << std::endl;
+	}
+};
+
+class Bank {
+private:
+	std::vector <Client*>_clients;
+public:
+
+	void AddClient(Client* client) {
+		_clients.push_back(client);
+	}
+
+	void ShowClients() {
+		// громоздкий вариант
+		/*std::vector<Client*>::iterator it;
+		for (it = _clients.begin(); it != _clients.end(); ++it)
+			(*it)->ShowData();*/
+
+		//Быстрый вариант. Используется с "диапазонными" данными
+		for(const auto & it : _clients)
+			it->ShowData();
+	}
+
+	void ShowClient(int index) {
+		if (index > _clients.size())
+			return;
+		_clients.at(index - 1)->ShowData();
+	}
+};
 
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-
+	// Задачка 1
+	/*
 	Person P1;
 	P1.SetName("Санёк");
 	P1.SetAge(14);
@@ -108,9 +154,30 @@ int main() {
 
 	Professor Pr1("Александр", "Доктор наук");
 	Pr1.PrintAll();
-
+	*/
 	
+	// Задачка 2
+	
+	//Bank bankx;
+	//bankx.AddClient();
 
+	Bank* bank = new Bank();
+	Client* client1 = new Client("Александр", 14, 15000);
+	Client* client2 = new Client("Санек", 18, 1123);
+	Client* client3 = new Client("Шурик", 25, 100);
+	Client* client4 = new Client("Искандер", 43, 5000);
+	Client* client5 = new Client("Александр Македонский", 726, 4500);
+	Client* client6 = new Client("Санчо Пансо", 100, 25);
+	
+	bank->AddClient(client1);
+	bank->AddClient(client2);
+	bank->AddClient(client3);
+	bank->AddClient(client4);
+	bank->AddClient(client5);
+	bank->AddClient(client6);
+
+	bank->ShowClients();
+	bank->ShowClient(2);
 
 
 
